dd305cab962aff6032afda9ce58a8e9a0bee5110
Phong <phong940253@gmail.com>
2022-03-04 09:53:24 +0700

    Add position background

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,10 +7,26 @@ public class Spawner : MonoBehaviour
     public GameObject fish;
     public float spawnTime = 1f;
     public int maxFish = 3, numFish = 0;
+    public float sizeMap = 7f, sizeScreen = 1.4f;
+
+    public Vector3 spawnPosition;
     // Start is called before the first frame update
     void Start()
-    {
+    {  
+        spawnPosition = transform.position;
+        spawnPosition.x = 1.4f;
         InvokeRepeating("createFish", spawnTime, spawnTime);
+    }
+
+    Vector3 RandomPosition(Vector3 position) {
+        float x, y;
+        do {
+            x = Random.Range(-sizeMap, sizeMap);
+            y = Random.Range(-sizeMap, sizeMap); 
+        } while (!(-sizeMap < x && x < -sizeScreen && sizeScreen < x && x < sizeMap && -sizeMap < y && y < -sizeScreen && sizeScreen < y && y < sizeMap));
+        position.x = x;
+        position.y = y;
+        return position;
     }
 
     // Update is called once per frame
@@ -21,7 +37,7 @@ public class Spawner : MonoBehaviour
 
     void createFish() {
         if (numFish < maxFish) {
-            Instantiate(fish, transform.position, transform.rotation, GameObject.Find("Fishs").transform);    
+            Instantiate(fish, spawnPosition, transform.rotation, GameObject.Find("Fishs").transform);    
             numFish++;
         }
           
